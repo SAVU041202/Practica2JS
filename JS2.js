3229b2dd7b8a37bb5f030a82d95767f8147bcbaa
SAVU041202 <sergioalex_vu04@hotmail.com>
2022-10-28 09:53:50 -0500

    Se Actualizo JS

---
+++ JS2.js
@@ -0,0 +1,44 @@
+let alumnos = ("mario,luis");
+
+console.log(alumnos);
+console.log("Total elementos",
+alumnos[0]);
+
+console.log("Primer elemento",
+alumnos[-1]);
+
+console.log("Ultimo elemento",
+alumnos[alumnos.length-1]);
+
+console.log("Tipo de dato",
+typeof alumnos);
+
+let grupo51 = {
+    "nombre" : "grupo 51",
+    "semestre" : 5,
+    "carrera" : "LTI",
+    "alumnos" : alumnos
+
+};
+
+console.log("Nombre",
+grupo51["nombre"],
+grupo51.nombre);
+
+console.log("Primer alumno",
+grupo51.alumnos[0]
+);
+
+document.write("<marquee>mensaje</marquee>");
+document.write('<ul><li>Elementos</li></ul>');
+
+
+/*Ejemplo if */
+
+let calif = 45;
+
+if (calif >= 95 && calif <= 100) {
+console.log("Excelente");
+} else {
+    console.log("No Excelente")
+}
